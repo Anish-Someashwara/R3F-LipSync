2deb8a038394d24f5170f766e7db911cac3c61c4
Anish Kumar <anishkumar.singh@someshwara.com>
2024-05-16 20:05:17 +0530

    Added face expressions

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,9 +8,22 @@ export const Experience = () => {
 
   return (
     <>
-      {/* <OrbitControls /> */}
+      <OrbitControls
+        enablePan={true}
+        enableZoom={true}
+        enableRotate={true}
+        enableDamping={true} // This enables smooth damping of the controls
+        dampingFactor={0.1} // Adjust the damping factor to control the smoothness
+        zoomSpeed={0.5} // Adjust the zoom speed for smoother zooming
+        rotateSpeed={0.5} // Adjust the rotate speed for smoother rotation
+        panSpeed={0.5} // Adjust the pan speed for smoother panning
+      />
       <Avatar position={[0, -3, 5]} scale={2} />
-      <Environment preset="sunset" />
+      {/* <Environment preset="sunset" background blur={0} /> */}
+      {/* <Environment preset="sunset"/> */}
+      <directionalLight position={[0, 1.0, 4.4]} intensity={0.9} />
+      {/* <directionalLight position={[-3.3, 1.0, 4.4]} intensity={0.5} /> */}
+      {/* <ambientLight intensity={0.1}/> */}
       {/* <mesh>
         <planeGeometry args={[viewport.width, viewport.height]} />
         <meshBasicMaterial map={texture} />
