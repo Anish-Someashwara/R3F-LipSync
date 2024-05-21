e84c6d7e3b764f7dfd6612f8f13892c8a047d594
Anish Kumar <anishkumar.singh@someshwara.com>
2024-05-21 12:06:04 +0530

    Added facial Expressions with shapekeys

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,44 +3,109 @@ import { useFrame, useLoader } from "@react-three/fiber";
 import { useControls } from "leva";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
-import { ConvolutionShader } from "three/examples/jsm/shaders/ConvolutionShader.js";
+import facialExpressions from "./facialExpressions.json"
+
+
+// const facialExpressions = {
+//   default: {},
+//   Angry: {
+//     "Angry": 0.7
+//   },
+//   Surprise: {
+//     "Surprise": 1
+//   },
+//   Sad: {
+//     "Sad": 1,
+//     // "Lip_Sad": 0.55
+//   },
+//   Smile: {
+//     "Smile": 1,
+//     "Lip_Smile": 0.2
+//   },
+//   IdleSmile: {
+//     "Lip_Smile": 0
+//   },
+//   HappySurprised: {
+//     "Surprise": 1,
+//     "Lip_Smile": 0.5
+//   },
+//   Surprise_1: {
+//     "Surprise": 1,
+//   },
+//   Surprise_09: {
+//     "Surprise": 0.9,
+//   },
+//   Surprise_08: {
+//     "Surprise": 0.8,
+//   },
+//   Surprise_07: {
+//     "Surprise": 0.7,
+//   },
+//   Surprise_06: {
+//     "Surprise": 0.6,
+//   },
+//   Surprise_05: {
+//     "Surprise": 0.5,
+//   },
+//   Surprise_04: {
+//     "Surprise": 0.4,
+//   },
+//   Surprise_03: {
+//     "Surprise": 0.3,
+//   },
+//   Surprise_02: {
+//     "Surprise": 0.2,
+//   },
+//   Surprise_01: {
+//     "Surprise": 0.1,
+//   },
+//   Surprise_00: {
+//     "Surprise": 0,
+//   },
+
+//   SurpriseSad_08: {
+//     "Surprise": 0.8,
+//     "Lip_Sad": 0.6
+//   },
+
+//   Angry_1:{
+//     "Angry": 1,
+//   },
+//   Angry_09:{
+//     "Angry": 0.9
+//   },
+//   Angry_08:{
+//     "Angry": 0.8
+//   },
+//   Angry_07:{
+//     "Angry": 0.7
+//   },
+//   Angry_06:{
+//     "Angry": 0.6
+//   },
+//   Angry_05:{
+//     "Angry": 0.5
+//   },
+//   Angry_04:{
+//     "Angry": 0.4
+//   },
+//   Angry_03:{
+//     "Angry": 0.3
+//   },
+//   Angry_02:{
+//     "Angry": 0.2
+//   },
+//   Angry_01:{
+//     "Angry": 0.1
+//   },
+//   Angry_00:{
+//     "Angry": 0
+//   }
+  
+// }
 
 
 
-const facialExpressions = {
-  default: {},
-  Angry: {
-    "Angry_2": 0.7
-  },
-  // EyeBlink: {
-  //   "Eyeblink_2": 1
-  // },
-  Surprise: {
-    "Surprise_2": 1
-  },
-  Sad: {
-    "Sad_2": 1,
-    "Lip_Sad": 0.55
-  },
-  Smile: {
-    "Smile_2": 1,
-    "Lip_Smile": 0.5
-  },
-  // funnyFace: {
-  //   jawLeft: 0.63,
-  //   mouthPucker: 0.53,
-  //   noseSneerLeft: 1,
-  //   noseSneerRight: 0.39,
-  //   mouthLeft: 1,
-  //   eyeLookUpLeft: 1,
-  //   eyeLookUpRight: 1,
-  //   cheekPuff: 0.9999924982764238,
-  //   mouthDimpleLeft: 0.414743888682652,
-  //   mouthRollLower: 0.32,
-  //   mouthSmileLeft: 0.35499733688813034,
-  //   mouthSmileRight: 0.35499733688813034,
-  // },
-}
 
 // const corresponding = {
 //   A: "viseme_PP",              // P_B_M
@@ -68,15 +133,34 @@ const corresponding = {
 };
 
 export function Avatar(props) {
-  console.log("fldkfjdkfj")
-  const { nodes, scene, materials, animations: smileAnimation } = useGLTF("./models/FacialExp7.gltf");
-  // console.log(smileAnimation)
-  // const [animation, setAnimation] = useState("Eyeblink_2");
+  // console.log("fldkfjdkfj")
+
+
+  const { nodes, scene, materials, animations: smileAnimation } = useGLTF("./models/FacialExp9.gltf");
   const group = useRef();
-  // const { actions } = useAnimations(
-  //   smileAnimation,
-  //   group
-  // );
+  const { actions } = useAnimations( smileAnimation, group);
+  const [animation, setAnimation] = useState("Idle");
+  // console.log(smileAnimation)
+
+  // Play idle animation continuously
+  // useEffect(() => {
+  //   actions["Idle"].reset().fadeIn(0.5).play();
+  //   return () => actions["Idle"].fadeOut(0.5);
+  // }, [actions]);
+
+  // Play selected animation when it changes
+  useEffect(() => {
+    if (animation !== "Idle") {
+      actions[animation].reset().fadeIn(0.5).play();
+      return () => actions[animation].fadeOut(0.5);
+    }
+  }, [animation, actions]);
+
+  // useEffect(() => {
+  //   actions[animation].reset().fadeIn(0.5).play();
+  //   return () => actions[animation].fadeOut(0.5);
+  // }, [animation]);
+
   const [facialExpression, setFacialExpression] = useState("")
 
 
@@ -94,29 +178,18 @@ export function Avatar(props) {
     morphTargetSmoothing: 0.40,
     script: {
       value: "Bot-Intro-3",
-      options: ["Bot-Intro", "Bot-Intro-3", "pizzas"],
+      options: ["Bot-Intro", "Bot-Intro-3", "pizzas", "LotteryPrank"],
     },
-    // animations: {
-    //   value: animation,
-    //   options: smileAnimation.map((a) => a.name),
-    //   onChange: (value) => setAnimation(value),
-    // },
+    animations: {
+      value: animation,
+      // options: smileAnimation.map((a) => a.name),
+      options: smileAnimation.map(a => a.name).filter(name => name !== "Idle"),
+      onChange: (value) => setAnimation(value),
+    },
     facialExpression: {
       options: Object.keys(facialExpressions),
       onChange: (value) => {
         console.log("Changing Face Exp!", value)
-        // !false &&
-        // Object.keys(nodes.Face.morphTargetDictionary).forEach((key) => {
-        //   const mapping = facialExpressions[value];
-        //   if (key === "Eyeblink_2" || key === "Eyeblink_2") { return;}
-
-        //   if (mapping && mapping[key]) {
-        //     console.log("changing: ", mapping, mapping[key])
-        //     lerpMorphTarget(key, mapping[key], 0.1);
-        //   } else {
-        //     lerpMorphTarget(key, 0, 0.1);
-        //   }
-        // });
         setFacialExpression(value)
       },
     },
@@ -126,18 +199,20 @@ export function Avatar(props) {
   const jsonFile = useLoader(THREE.FileLoader, `./audios/${script}.json`);
   const lipsync = JSON.parse(jsonFile);
 
+  
+
   useFrame(() => {
     ///////// exp1
     !false &&
     Object.keys(nodes.Face.morphTargetDictionary).forEach((key) => {
       const mapping = facialExpressions[facialExpression];
-      if (key === "Eye_Blink_2") { return; }
+      if (key === "Eye_Blink") { return; }
 
       if (mapping && mapping[key]) {
         // console.log("changing: ", mapping, mapping[key])
-        lerpMorphTarget(key, mapping[key], 0.1);
+        lerpMorphTarget(key, mapping[key], 0.05);
       } else {
-        lerpMorphTarget(key, 0, 0.1);
+        lerpMorphTarget(key, 0, 0.05);
       }
     });
     ///////// exp1
@@ -159,6 +234,8 @@ export function Avatar(props) {
     // console.log("HIIi")
     const currentAudioTime = audio.currentTime;
     if (audio.paused || audio.ended) {
+      // if(audio.ended) playAudio = false;
+      setFacialExpression('IdleSmile')
       return;
     }
 
@@ -200,7 +277,8 @@ export function Avatar(props) {
         currentAudioTime >= mouthCue.start &&
         currentAudioTime <= mouthCue.end
       ) {
-        // console.log("mouthCue: ", mouthCue.value)
+        console.log("setting face exp: ", mouthCue.exp)
+        setFacialExpression(mouthCue.exp)
         if (!smoothMorphTarget) {
           nodes.Face.morphTargetInfluences[
             nodes.Face.morphTargetDictionary[
@@ -244,7 +322,9 @@ export function Avatar(props) {
     //       if (value === cueValue && currentAudioTime >= mouthCue.start && currentAudioTime <= mouthCue.end) {
     //         const intensity = mouthCue.intensity || 1; // Default intensity if not provided
     //         acc = intensity;
+    //         setFacialExpression(mouthCue.exp)
     //       }
+
     //       return acc;
     //     }, 0);
         
@@ -292,20 +372,20 @@ export function Avatar(props) {
   // ********************************** Eyes Blink Animation **********************************
 
     ////// EyeBlink-1
-  useEffect(()=>{
-    setInterval(() => {
-      // console.log("****** Eye Blinked! ******")
-      const closeFunc = () => {
-        nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]] = 0;
-      };
+  // useEffect(()=>{
+  //   setInterval(() => {
+  //     // console.log("****** Eye Blinked! ******")
+  //     const closeFunc = () => {
+  //       nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]] = 0;
+  //     };
 
-      setTimeout(() => {
-        nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]] = 1;
-        setTimeout(closeFunc, 200);
-      }, THREE.MathUtils.randInt(3500, 7500));
+  //     setTimeout(() => {
+  //       nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]] = 1;
+  //       setTimeout(closeFunc, 200);
+  //     }, THREE.MathUtils.randInt(3500, 7500));
       
-    }, 5000);
-  },[])
+  //   }, 5000);
+  // },[])
     ////// EyeBlink-1
 
   
@@ -355,53 +435,53 @@ export function Avatar(props) {
 
 
 
-  // useEffect(() => {
-  //   const blink = () => {
-  //     const closeDuration = THREE.MathUtils.randInt(100, 200); // Short duration for closing
-  //     const closeStart = Date.now();
-  //     const closeEnd = closeStart + closeDuration;
-  //     const openDuration = THREE.MathUtils.randInt(100, 200); // Short duration for opening
-  //     const openStart = closeEnd; // Start opening right after closing ends
-  //     const openEnd = openStart + openDuration;
-  //     const initialWeight = nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]];
-  //     const targetWeight = 1; // Always close the eyes
+  useEffect(() => {
+    const blink = () => {
+      const closeDuration = THREE.MathUtils.randInt(100, 200); // Short duration for closing
+      const closeStart = Date.now();
+      const closeEnd = closeStart + closeDuration;
+      const openDuration = THREE.MathUtils.randInt(100, 200); // Short duration for opening
+      const openStart = closeEnd; // Start opening right after closing ends
+      const openEnd = openStart + openDuration;
+      const initialWeight = nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]];
+      const targetWeight = 1; // Always close the eyes
   
-  //     const updateMorphTarget = () => {
-  //       const now = Date.now();
-  //       let progress, weight;
+      const updateMorphTarget = () => {
+        const now = Date.now();
+        let progress, weight;
   
-  //       if (now < closeEnd) {
-  //         // Closing phase
-  //         progress = Math.min(1, (now - closeStart) / closeDuration);
-  //         weight = THREE.MathUtils.lerp(initialWeight, targetWeight, progress);
-  //       } else if (now < openEnd) {
-  //         // Opening phase
-  //         progress = Math.min(1, (now - openStart) / openDuration);
-  //         weight = THREE.MathUtils.lerp(targetWeight, initialWeight, progress);
-  //       } else {
-  //         // Reset the morph target influence to its initial value after the animation completes
-  //         nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]] = initialWeight;
-  //         return; // Blink animation finished
-  //       }
+        if (now < closeEnd) {
+          // Closing phase
+          progress = Math.min(1, (now - closeStart) / closeDuration);
+          weight = THREE.MathUtils.lerp(initialWeight, targetWeight, progress);
+        } else if (now < openEnd) {
+          // Opening phase
+          progress = Math.min(1, (now - openStart) / openDuration);
+          weight = THREE.MathUtils.lerp(targetWeight, initialWeight, progress);
+        } else {
+          // Reset the morph target influence to its initial value after the animation completes
+          nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]] = initialWeight;
+          return; // Blink animation finished
+        }
   
-  //       nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]] = weight;
-  //       requestAnimationFrame(updateMorphTarget);
-  //     };
+        nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]] = weight;
+        requestAnimationFrame(updateMorphTarget);
+      };
   
-  //     updateMorphTarget();
-  //   };
+      updateMorphTarget();
+    };
   
-  //   const blinkLoop = () => {
-  //     blink();
-  //     setTimeout(() => {
-  //       // Restore the initial morph target influence after a short delay to ensure eyes fully open
-  //       nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink_2"]] = 0;
-  //     }, 500); // Adjust delay as needed
-  //     setTimeout(blinkLoop, THREE.MathUtils.randInt(2000, 4000)); // Interval between blinks
-  //   };
+    const blinkLoop = () => {
+      blink();
+      setTimeout(() => {
+        // Restore the initial morph target influence after a short delay to ensure eyes fully open
+        nodes.Face.morphTargetInfluences[nodes.Face.morphTargetDictionary["Eye_Blink"]] = 0;
+      }, 500); // Adjust delay as needed
+      setTimeout(blinkLoop, THREE.MathUtils.randInt(2000, 4000)); // Interval between blinks
+    };
   
-  //   blinkLoop();
-  // }, []);
+    blinkLoop();
+  }, []);
   
   
   
@@ -415,15 +495,16 @@ export function Avatar(props) {
   // }, [animation]);
 
   // Find the EyeBlink_2 animation clip
-  // const eyeBlinkClip = smileAnimation.find((clip) => clip.name === "Eyeblink_2");
+  // const idelClip = smileAnimation.find((clip) => clip.name === "Idle");
 
-  // // // Adjust the time scale of EyeBlink_2 animation
+  // // Adjust the time scale of EyeBlink_2 animation
   // useEffect(() => {
-  //   if (eyeBlinkClip) {
-  //     eyeBlinkClip.timeScale = 0.1; // Set the desired time scale
-  //     eyeBlinkClip.duration = 2; // Set the desired time scale
+  //   if (idelClip) {
+  //     console.log("********************")
+  //     idelClip.timeScale = 1; // Set the desired time scale
+  //     // idelClip.duration = 2; // Set the desired time scale
   //   }
-  // }, [eyeBlinkClip]);
+  // }, [idelClip]);
   ////// EyeBlink-2
   
 
@@ -486,82 +567,84 @@ export function Avatar(props) {
 
 
   // *********************** Morph Target Controls ***********************
-
+  // const [, set] = useControls("MorphTarget", () =>
+  //   Object.assign(
+  //     {},
+  //     ...Object.keys(nodes.Face.morphTargetDictionary).map((key) => {
+  //       return {
+  //         [key]: {
+  //           label: key,
+  //           value: 0,
+  //           min: nodes.Face.morphTargetInfluences[
+  //             nodes.Face.morphTargetDictionary[key]
+  //           ],
+  //           max: 1,
+  //           onChange: (val) => {
+  //             if (true) {
+  //               // console.log("HII", key, val)
+  //               lerpMorphTarget(key, val, 1);
+  //             }
+  //           },
+  //         },
+  //       };
+  //     })
+  //   )
+  // );
 
 
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <mesh
-          name="Face"
-          castShadow
-          receiveShadow
-          geometry={nodes.Face.geometry}
-          material={materials['Character_A1:Bodymat']}
-          morphTargetDictionary={nodes.Face.morphTargetDictionary}
-          morphTargetInfluences={nodes.Face.morphTargetInfluences}
-          position={[-0.002, 1.569, -0.007]}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.105}
-        />
-        <mesh
-          name="Eyes"
-          castShadow
-          receiveShadow
-          geometry={nodes.Eyes.geometry}
-          material={materials['Character_A1:Bodymat']}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
-        />
-        <mesh
-          name="Hair"
-          castShadow
-          receiveShadow
-          geometry={nodes.Hair.geometry}
-          material={materials.Hairmat1}
-          position={[-0.002, 1.569, -0.007]}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.105}
-        />
-        <mesh
-          name="T_Shirt"
-          castShadow
-          receiveShadow
-          geometry={nodes.T_Shirt.geometry}
-          material={materials['Character_A1:Topmat']}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
-        />
-        <mesh
-          name="Shoes"
-          castShadow
-          receiveShadow
-          geometry={nodes.Shoes.geometry}
-          material={materials['Character_A1:Shoesmat']}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
-        />
-        <mesh
-          name="Pant"
-          castShadow
-          receiveShadow
-          geometry={nodes.Pant.geometry}
-          material={materials['Character_A1:Bottommat']}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
-        />
-        <mesh
-          name="Body"
-          castShadow
-          receiveShadow
-          geometry={nodes.Body.geometry}
-          material={materials['Character_A1:Bodymat']}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
-        />
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <skinnedMesh
+            name="Body"
+            geometry={nodes.Body.geometry}
+            material={materials.Bodymat3}
+            skeleton={nodes.Body.skeleton}
+          />
+          <skinnedMesh
+            name="Eyes"
+            geometry={nodes.Eyes.geometry}
+            material={materials.Bodymat3}
+            skeleton={nodes.Eyes.skeleton}
+          />
+          <skinnedMesh
+            name="Face"
+            geometry={nodes.Face.geometry}
+            material={materials.Bodymat3}
+            skeleton={nodes.Face.skeleton}
+            morphTargetDictionary={nodes.Face.morphTargetDictionary}
+            morphTargetInfluences={nodes.Face.morphTargetInfluences}
+          />
+          <skinnedMesh
+            name="Hair"
+            geometry={nodes.Hair.geometry}
+            material={materials.Hairmat1}
+            skeleton={nodes.Hair.skeleton}
+          />
+          <skinnedMesh
+            name="Pant"
+            geometry={nodes.Pant.geometry}
+            material={materials['Character_A1:Bottommat']}
+            skeleton={nodes.Pant.skeleton}
+          />
+          <skinnedMesh
+            name="Shoes"
+            geometry={nodes.Shoes.geometry}
+            material={materials['Character_A1:Shoesmat']}
+            skeleton={nodes.Shoes.skeleton}
+          />
+          <skinnedMesh
+            name="T_Shirt"
+            geometry={nodes.T_Shirt.geometry}
+            material={materials['Character_A1:Topmat']}
+            skeleton={nodes.T_Shirt.skeleton}
+          />
+          <primitive object={nodes.mixamorigHips} />
+        </group>
       </group>
     </group>
   )
 }
 
-useGLTF.preload('./models/FacialExp7.gltf')
+useGLTF.preload('./models/FacialExp9.gltf')
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,30 +3,30 @@ import { useThree } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 
 export const Experience = () => {
-  // const texture = useTexture("textures/bg5.jpg");
-  // const viewport = useThree((state) => state.viewport);
+  const texture = useTexture("textures/bg4.jpg");
+  const viewport = useThree((state) => state.viewport);
 
   return (
     <>
       <OrbitControls
         enablePan={true}
         enableZoom={true}
-        enableRotate={true}
-        enableDamping={true} // This enables smooth damping of the controls
+        enableRotate={false}
+        enableDamping={false} // This enables smooth damping of the controls
         dampingFactor={0.1} // Adjust the damping factor to control the smoothness
         zoomSpeed={0.5} // Adjust the zoom speed for smoother zooming
         rotateSpeed={0.5} // Adjust the rotate speed for smoother rotation
         panSpeed={0.5} // Adjust the pan speed for smoother panning
       />
       <Avatar position={[0, -3, 5]} scale={2} />
-      {/* <Environment preset="sunset" background blur={0} /> */}
+      <Environment preset="sunset" background blur={1} />
       {/* <Environment preset="sunset"/> */}
-      <directionalLight position={[0, 1.0, 4.4]} intensity={0.9} />
+      {/* <directionalLight position={[0, 1.0, 4.4]} intensity={0.2} /> */}
       {/* <directionalLight position={[-3.3, 1.0, 4.4]} intensity={0.5} /> */}
       {/* <ambientLight intensity={0.1}/> */}
       {/* <mesh>
         <planeGeometry args={[viewport.width, viewport.height]} />
-        <meshBasicMaterial map={texture} />
+        <meshBasicMaterial map={texture}  />
       </mesh> */}
     </>
   );
